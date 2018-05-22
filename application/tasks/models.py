30db2c1ef3c5a18430e071bb232db162ba588e88
Olli Rousu <olli.rousu@hotmail.com>
2018-05-22 11:10:10 +0300

    ensimmäinen tiatokantataulu tehty

---
+++ application/tasks/models.py
@@ -0,0 +1,14 @@
+from application import db
+
+class Thread(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+    onupdate=db.func.current_timestamp())
+
+    title = db.Column(db.String(144), nullable=False)
+    text = db.Column(db.String(400), nullable=False)
+
+    def __init__(self, name):
+        self.title = title
+        self.text = text
