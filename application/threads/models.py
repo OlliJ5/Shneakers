ae1c12a7bf8b02511d092fb41eaf71ab0b22d63d
Olli Rousu <olli.rousu@hotmail.com>
2018-05-30 20:33:13 +0300

    kirjautuminen lisätty

--- application/threads/models.py
+++ application/threads/models.py
@@ -4,7 +4,7 @@ class Thread(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
     date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-    onupdate=db.func.current_timestamp())
+                                onupdate=db.func.current_timestamp())
 
     title = db.Column(db.String(144), nullable=False)
     text = db.Column(db.String(400), nullable=False)
