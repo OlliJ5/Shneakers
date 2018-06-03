a7969d573df49f3d089aa0062b4e76c9055b2c15
Olli Rousu <olli.rousu@hotmail.com>
2018-06-04 01:56:00 +0300

    heroku db säätöö

--- application/__init__.py
+++ application/__init__.py
@@ -3,9 +3,15 @@ app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///shneakers.db"
-app.config["SQLALCHEMY_ECHO"] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///shneakers.db"
+    app.config["SQLALCHEMY_ECHO"] = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
 
 db = SQLAlchemy(app)
 
@@ -35,4 +41,7 @@ login_manager.login_message = "Please login to use this functionality."
 def load_user(user_id):
     return User.query.get(user_id)
 
-db.create_all()
+try:
+    db.create_all()
+except:
+    pass
