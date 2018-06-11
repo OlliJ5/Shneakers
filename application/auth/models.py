1f54d1cb6ee32b7e01e00dd20764324f52366be0
Olli Rousu <olli.rousu@hotmail.com>
2018-06-11 12:50:53 +0300

    yhteenvetokysely

--- application/auth/models.py
+++ application/auth/models.py
@@ -1,6 +1,8 @@
 from application import db
 from application.models import Base
 
+from sqlalchemy.sql import text
+
 class User(Base):
 
     __tablename__ = "account"
@@ -28,3 +30,17 @@ class User(Base):
 
     def is_authenticated(self):
         return True
+
+    @staticmethod
+    def find_how_many_tasks_each_user_has():
+        stmt = text("SELECT Account.id, Account.username, COUNT(Thread.id) FROM Account"
+                    " LEFT JOIN Thread ON Thread.account_id = Account.id"
+                    " GROUP BY Account.id")
+
+        res = db.engine.execute(stmt)
+
+        response = []
+        for row in res:
+            response.append({"id":row[0], "username":row[1], "posts":row[2]})
+
+        return response
